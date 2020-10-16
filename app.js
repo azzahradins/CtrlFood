cfd066d6052664590c5f62c2ba44404bf318ff2b
Azzahra Dinda S <azzahradindas@gmail.com>
2020-10-16 22:41:35 +0700

    Add configuration for connect to database

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-
+const mongoose = require('mongoose');
+require('dotenv/config')
 //Middlewares
 //Used when the routes is accessed by users.
 
@@ -13,6 +14,12 @@ app.get('/posts', (req, res) => {
     res.send("Another endpoints")
 });
 
+//connect to db
+mongoose.connect(process.env.DB_CONNECTION, 
+{useNewUrlParser: true, useUnifiedTopology: true},
+() =>
+    console.log('connected to db!')
+);
 
 //Now start listening on 3000
 app.listen(3000);
